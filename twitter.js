8e219f6be508b60124abb3903f33172cd4df9521
Ivan Harasta <ivan.harasta@footshop.cz>
2018-02-16 11:45:27 +0100

    GET request to twitter api example

--- twitter.js
+++ twitter.js
@@ -4,3 +4,9 @@ const Twit = require('twit')
 
 const config = require('./config')
 const T = new Twit(config)
+
+let tweets = []
+
+T.get('search/tweets', {q: 'banana since:2018-2-1', count: 5}, (err, data, response) => {
+  console.log(data)
+})
